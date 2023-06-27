6d953fb9e489cd7b420716bd1c5bad00f44cac56
Asifur Rahman <asifur94@outlook.com>
2023-06-27 15:06:01 +0600

    javascript problem solved

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,42 +11,60 @@
       $('.offcanvase_button').click(function(){
         $('.site_left_offcanvase').addClass('active');
         $('.chat_content_box').addClass('active');
-      })
+      });
+
+      $('.popup_active').click(function(){
+        $('.user_profile').toggleClass('active');
+        $('.setting_for_site').removeClass('active');
+        $('.sign_in_options').removeClass('active');
+      });
+
+      $('.settings').click(function(){
+        $('.user_profile').removeClass('active');
+        $('.setting_for_site').toggleClass('active');
+        $('.sign_in_options').removeClass('active');
+      });
+
+      $('.sign-in').click(function(){
+        $('.user_profile').removeClass('active');
+        $('.setting_for_site').removeClass('active');
+        $('.sign_in_options').toggleClass('active');
+      });
 
     })
 
    
 
-    const bodyclass = document.querySelector('body');
-    const popup = document.querySelector('.popup_active');
-    const user = document.querySelector('.user_profile');
+    // const bodyclass = document.querySelector('body');
+    // const popup = document.querySelector('.popup_active');
+    // const user = document.querySelector('.user_profile');
 
-    popup.addEventListener('click', function(e){
-      e.preventDefault();
-      user.classList.toggle('active');
-      settings.classList.remove('active');
-      options.classList.remove('active')
-    });
+    // popup.addEventListener('click', function(e){
+    //   e.preventDefault();
+    //   user.classList.toggle('active');
+    //   settings.classList.remove('active');
+    //   options.classList.remove('active')
+    // });
     
-    const setting = document.querySelector('.settings');
-    const settings = document.querySelector('.setting_for_site');
+    // const setting = document.querySelector('.settings');
+    // const settings = document.querySelector('.setting_for_site');
 
-    setting.addEventListener('click', function(e){
-      e.preventDefault();
-      settings.classList.toggle('active');
-      user.classList.remove('active');
-      options.classList.remove('active')
-    });
+    // setting.addEventListener('click', function(e){
+    //   e.preventDefault();
+    //   settings.classList.toggle('active');
+    //   user.classList.remove('active');
+    //   options.classList.remove('active')
+    // });
 
-    const signin = document.querySelector('.sign-in');
-    const options = document.querySelector('.sign_in_options');
+    // const signin = document.querySelector('.sign-in');
+    // const options = document.querySelector('.sign_in_options');
 
-    signin.addEventListener('click', function(e){
-      e.preventDefault();
-      options.classList.toggle('active')
-      user.classList.remove('active');
-      settings.classList.remove('active');
-    });
+    // signin.addEventListener('click', function(e){
+    //   e.preventDefault();
+    //   options.classList.toggle('active')
+    //   user.classList.remove('active');
+    //   settings.classList.remove('active');
+    // });
 
 
    
